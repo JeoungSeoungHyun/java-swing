b8a3b8a7a176275c44d9f138435b2ef6d7ecbbd9
tjdgus <tjdgus7562@naver.com>
2022-02-09 23:07:32 +0900

    프레임에 배경화면 넣기

--- src/site/metacoding/bubble/test2/BubbleFrame.java
+++ src/site/metacoding/bubble/test2/BubbleFrame.java
@@ -1,25 +1,32 @@
 package site.metacoding.bubble.test2;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  * 
- * @author 정성현 / 목적 : 기본 프레임 만들기
+ * @author 정성현 / 목적 : 프레임에 배경화면 넣기
  *
  */
 public class BubbleFrame extends JFrame {
+
+	private JLabel backgroundMap;
 
 	// 생성자를 통해 프레임 생성 및 설정
 	public BubbleFrame() {
 		initObject();
 		initSetting();
+		addObject();
 		setVisible(true);
 	}
 
+	// 오브젝트 생성 메서드
 	private void initObject() {
-
+		backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
 	}
 
+	// 세팅 메서드
 	private void initSetting() {
 		// 프레임 사이즈 설정
 		setSize(1000, 640);
@@ -33,6 +40,11 @@ public class BubbleFrame extends JFrame {
 		// x키 클릭시 JVM 종료
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+	}
+
+	// 오브젝트 추가 메서드
+	private void addObject() {
+		setContentPane(backgroundMap);
 	}
 
 	public static void main(String[] args) {
