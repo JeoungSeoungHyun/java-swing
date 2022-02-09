8279bff61162c6602afe7f92e56d8726ef4f44e3
tjdgus7562 <tjdgus7562@naver.com>
2022-02-09 12:11:27 +0900

    플레이어생성

--- src/site/metacoding/test/BubbleFrame.java
+++ src/site/metacoding/test/BubbleFrame.java
@@ -4,26 +4,33 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
+import site.metacoding.bubble.ex02.Player;
+
 /**
  * 
- * @author 정성현 목적 : 프레임에 배경화면 추가
+ * @author 정성현 목적 : 플레이어 생성
  *
  */
 public class BubbleFrame extends JFrame {
 
 	private JLabel backgroundMap;
+	private JLabel player;
 
 	// 1. 생성자를 통해 프레임을 생성한다.
 	public BubbleFrame() {
 		initSetting();
 		initObject();
+		addObject();
 		setVisible(true);
 	}
 
 	// 오브젝트 생성 메서드
 	private void initObject() {
+		// 배경화면을 위한 레이블 생성 후 배경화면에 추가
 		backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
-		setContentPane(backgroundMap);
+
+		// 플레이어 생성
+		player = new Player();
 	}
 
 	// 프레임세팅 메서드
@@ -39,6 +46,14 @@ public class BubbleFrame extends JFrame {
 
 		// 프레임 종료시 JVM도 같이 종료하도록 세팅
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void addObject() {
+		// 프레임에 스테이지 배경화면 추가
+		setContentPane(backgroundMap);
+
+		// 플레이어 추가
+		add(player);
 	}
 
 	public static void main(String[] args) {
--- src/site/metacoding/test/Player.java
+++ src/site/metacoding/test/Player.java
@@ -1,5 +1,33 @@
 package site.metacoding.test;
 
-public class Player {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Player extends JLabel {
+	// 플레이어 생성자를 통해 생성
+	private int x;
+	private int y;
+	private ImageIcon playerR;
+
+	public Player() {
+		initObject();
+		initSetting();
+	}
+
+	private void initObject() {
+		// 오른쪽 바라보는 이미지아이콘 생성
+		playerR = new ImageIcon("image/playerR.png");
+	}
+
+	private void initSetting() {
+		// add하기 위해 필요한 초기좌표 설정
+		x = 70;
+		y = 535;
+
+		// setIcon과 setSize로 원하는 이미지아이콘 설정 후 setLocation으로 다시 그려주기
+		setIcon(playerR);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
 
 }
